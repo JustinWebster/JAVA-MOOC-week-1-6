532c14fa2bfa8d7981cd80829fde7e46cb8dd188
Justin Webster <justinwebberwebster@gmail.com>
2019-10-13 19:04:59 +0200

    Creating classes

---
+++ week4-074.Multiplier/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+//tested to make it was working
+    public static void main(String[] args) {
+        Multiplier time = new Multiplier(4);
+        System.out.println(time.multiply(2));
+    }
+}
